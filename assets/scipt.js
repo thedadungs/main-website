8073acce666be5210249f8b18ce6165788b6a87b
Thedadungs <danupristowo09@gmail.com>
2021-10-27 19:02:53 +0700

    form validation

--- assets/scipt.js
+++ assets/scipt.js
@@ -1,5 +1,6 @@
 const openRegisterButtons = document.querySelectorAll('[data-register-open]')
 const closeProjectButtons = document.querySelectorAll('[data-close-button]')
+const formControlValue = document.getElementsByClassName('form-control')
 const overlay = document.getElementById('overlay')
 const overlay2 = document.getElementById('overlay2')
 const body = document.getElementById('body')
@@ -34,6 +35,12 @@ function closePopUp(x) {
   overlay.classList.remove('active')
   body.classList.remove('active')
   clearForms($('#user'));
+  formControlValue[0].classList.remove('fill','wrong','success');
+  formControlValue[1].classList.remove('fill','wrong','success');
+  formControlValue[2].classList.remove('fill','wrong','success');
+  formControlValue[3].classList.remove('fill','wrong','success');
+  formControlValue[4].classList.remove('fill','wrong','success');
+  formControlValue[5].classList.remove('fill','wrong','success');
 }
 
 function myFunction() {
@@ -50,6 +57,7 @@ function clearForms()
 {
     $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
     $(':checkbox, :radio').prop('checked', false);
+    $('select').prop('selectedIndex', 0);
 }
 
 
@@ -93,3 +101,99 @@ overlay.addEventListener('click', () => {
 window.onscroll = function() {
   myFunction()
 }
+
+// validation
+const form = document.getElementById('submit')
+const username = document.getElementById('username')
+const school = document.getElementById('school');
+const email = document.getElementById('email');
+const metode = document.getElementById('payment');
+const bukti = document.getElementById('bukti');
+
+form.addEventListener('click', (x) =>{
+  x.preventDefault();
+  checkInput();
+});
+
+function checkInput(){
+  const usernameValue = username.value.trim();
+  const schoolValue = school.value.trim();
+  const emailValue = email.value.trim();
+  const phoneValue = phone.value.trim();
+  // const buktiValue = bukti.value.trim();
+
+  if(usernameValue === ''){
+    setErrorFor(username)
+  } 
+  // else if(usenameValue === wrong){
+  //   setWrongFor(username)
+  // }
+  else{
+    setSuccesFor(username)
+  }
+  if(schoolValue === ''){
+    setErrorFor(school)
+  } 
+  // else if(usenameValue === wrong){
+  //   setWrongFor(username)
+  // }
+  else{
+    setSuccesFor(school)
+  }
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} 
+  else if (!isEmail(emailValue)) {
+		setWrongFor(email, 'Not a valid email');
+	} 
+  else {
+		setSuccesFor(email);
+	}
+  if(phoneValue === ''){
+    setErrorFor(phone)
+  } 
+  else if((phoneValue.length > 13) || (phoneValue.length < 10) ){
+    setWrongFor(phone)
+  }
+  else{
+    setSuccesFor(phone)
+  }
+  if ($('#payment option:selected').val() == '-- Pilih Satu --') {
+    setErrorFor(metode)
+  } else {
+    setSuccesFor(metode)
+  }
+  if (bukti.value.length){
+    setSuccesFor(bukti)
+  } else {
+    setErrorFor(bukti)
+  }
+}
+
+function setErrorFor(input){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control fill';
+}
+function setWrongFor(input){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control wrong';
+}
+function setSuccesFor(input){
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+function validate() {
+  // check if input is bigger than 3
+  var value = document.getElementById('titleeee').value;
+  if (value.length < 3) {
+    return false; // keep form from submitting
+  }
+ 
+  // else form is good let it submit, of course you will 
+  // probably want to alert the user WHAT went wrong.
+ 
+  return true;
+ }
